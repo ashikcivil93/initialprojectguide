44f6b2ca2870cab1407cd47ac09d633209409338
ashikcivil93 <ashikcivil93@gmail.com>
2021-10-25 11:21:59 +0530

    push serious

---
+++ AshikJavaBasic/src/main/java/maven/java/project/AshikJavaBasic/serious.java
@@ -0,0 +1,38 @@
+package maven.java.project.AshikJavaBasic;
+
+public class serious {
+	public static void main(String[] args) {
+		
+		int Number = 1000;
+	/*
+		while (Number<=5000)
+		{
+			System.out.println(Number);
+		    Number++;
+	
+		}*/
+			
+	
+
+
+       int number=2000;
+       do
+       {
+    	   System.out.println(number);
+    	   number++;
+       }
+       while(number<=1500);
+	
+  for
+  
+  (number=1000;number<2000;number++)
+  {
+  System.out.println(number);
+  }
+	}
+}
+	
+	
+	
+
+	
