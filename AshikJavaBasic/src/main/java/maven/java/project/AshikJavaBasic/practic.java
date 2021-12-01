1abd5fcba57c12453327b3d3a5f6b5b558a7f7bc
ashikcivil93 <ashikcivil93@gmail.com>
2021-12-01 11:25:17 +0530

    push

---
+++ AshikJavaBasic/src/main/java/maven/java/project/AshikJavaBasic/practic.java
@@ -0,0 +1,31 @@
+package maven.java.project.AshikJavaBasic;
+
+import java.util.Scanner;
+
+public class practic {
+	public static void main(String[] args) {
+		
+ Scanner s=new Scanner(System.in);
+ System.out.println("my name is");
+ String name=s.nextLine();
+ System.out.println("date of birth");
+int  birthyear=s.nextInt();
+ System.out.println("your address");
+ String address=s.nextLine();
+ System.out.println("what you are doing");
+ String doing=s.nextLine();
+ System.out.println("what kind of work are you doing now");
+ String work=s.nextLine();
+ System.out.println("how many years are doing this business");
+ int years=s.nextInt();
+		 System.out.println("what is your future plan");
+		 String plan=s.nextLine();
+		 
+ 
+		 
+ 
+ 
+	
+}
+
+}
